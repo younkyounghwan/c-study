8ad0dd31cd121af8a585baabb814bc92c05797f8
younkyounghwan <youn7117@naver.com>
2019-09-01 08:50:39 +0900

    -190901-

---
+++ TrueAndFalse/TrueAndFalse/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 10;
+	int i = 0;
+
+	cout << "True: " << true << endl;
+	cout << "False: " << false << endl;
+
+	while (true)
+	{
+		cout << ++i << ' ';
+		if (i > num)
+			break;
+	}
+	cout << endl;
+
+	cout << "sizeof 1: " << sizeof(1) << endl;
+	cout << "sizeof 0: " << sizeof(0) << endl;
+	cout << "sizeof true: " << sizeof(true) << endl;
+	cout << "sizeof false: " << sizeof(false) << endl;
+	return 0;
+}
